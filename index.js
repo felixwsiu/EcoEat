82da676f2923c1ac5ae4c081ea4a30bf8cbe6fde
j1l1b <j1l1b@ugrad.cs.ubc.ca>
2019-11-22 02:58:30 -0800

    hidden paragragh to hold file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000
+const port = 2000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
 app.get('/retrieved', (req, res) => res.sendFile(path.join(__dirname+'/retrieved.js')))
