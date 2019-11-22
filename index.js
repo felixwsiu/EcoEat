e20fe2a7a995d777d8cc906c4ba212d025a4eb7f
James Shen <jamesshen09@gmail.com>
2019-11-22 01:48:03 -0800

    fixed package.json comma

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-//const router = app.Router();
-const port = 8000;
+const port = 3000
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
 app.get('/retrieved', (req, res) => res.sendFile(path.join(__dirname+'/retrieved.js')))
 app.get('/require', (req, res) => res.sendFile(path.join(__dirname+'/require.js')))
 app.get('*', function(req, res) {  res.render('error');});
 	
+
 
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
